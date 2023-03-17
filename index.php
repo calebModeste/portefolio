766ab4c40ac904d3be36d3ce850aa421ba6cc80e
modeste TSIBA <tsibacaleb@gmail.com>
2023-03-17 17:47:24 +0100

    mofofier

--- index.php
+++ index.php
@@ -11,6 +11,9 @@
     <title>Welcome</title>
 </head>
 <body>
+    <section class="grid">
+        
+    </section>
     <script src="js/main.js"></script>
     <header>
 
@@ -28,55 +31,47 @@
 
         <div class="menu" >
             <ul>
-                <a href="#Presentation"  >
-                    <li>
+                    <li onclick="nav()">
                         <div>
                             <img src="img/icon/utilisateur.png" alt="utilisateur"> 
                         </div> 
-                        <p  id="onOff"> Presentation  </p>
+                        <p> Presentation  </p>
                     </li>  
-                </a>
 
-                <a href="#BTS"  >
-                    <li>
+                    <li onclick="nav(show=2)">
                         <div>
                             <img src="img/icon/casquette-de-graduation.png" alt="etude"> 
                         </div> 
                         <p id="onOff"> BTS SIO SLAM </p> 
                     </li> 
-                </a>
 
-                <a href="#Realisation" >
-                    <li>
+                    <li onclick="nav(show=1)">
                         <div>
                             <img src="img/icon/applications.png" alt="projet"> 
                         </div> 
                         <p id="onOff"> Realisation </p>
                     </li>  
-                </a>
 
-                <a href="#Veille"  >
-                    <li>
+                    <li onclick="nav(show=3)">
                         <div>
                             <img src="img/icon/oeil.png" alt="recherche"> 
                         </div> 
                         <p id="onOff"> Veille </p> 
                     </li> 
-                </a>
 
             </ul>
         </div>
         <div class="Footer">
             <ul>
-                <a href="mailto:tsibacaleb@gmail.com"><li> <div><img src="img/icon/enveloppe1.png" alt="lettre"></div>  </li> <p> e-mail</p></a>
-                <a href="https://www.linkedin.com/in/caleb-nathanael-tsiba-modeste-88127a197"><li> <div><img src="img/icon/enveloppe1.png" alt="lettre"></div> </li><p> linkdl</p></a>
-                <a href="tel:0753886764"><li> <div><img src="img/icon/enveloppe1.png" alt="lettre"></div>  </li> <p> 07 53 88 67 64</p></a>
+                <a href="mailto:tsibacaleb@gmail.com"><li> mail </li> <p> </p></a>
+                <a href="https://www.linkedin.com/in/caleb-nathanael-tsiba-modeste-88127a197"><li> linkdl </li></a>
+                <a href="tel:0753886764"><li> phone  </li> </a>
             </ul>
             
         </div>
     </header>
     <main>
-        <section id="Presentation">
+        <section id="Presentation" onclick="navPresentation">
             <div class="Present-me">
                 <div >
                         <h4>DESCRIPTION</h4>
@@ -162,37 +157,10 @@
 
         </section>
         <section id="Realisation">
-                <div class="project">
-                    
-                        <img src="img/034166a5-9940-4c0c-9273-2a4dbe9d63a2.png" alt="projet image" width="250"  height="200">
-                    
-                    <div>
-                        <div class="description">
-                            <div>
-                                <p>Presentation:</p>
-                                <p>
-                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel maiores animi magnam repudiandae blanditiis laudantium asperiores doloremque voluptatum pariatur ea voluptatibus possimus nihil, eum, quisquam earum neque est laboriosam esse.
-                                </p>
-                            </div>
-                            <div>
-                                <p>techno:</p>
-                                <ul>
-                                    <li>techo1</li>
-                                    <li>techo1</li>
-                                    <li>techo1</li>
-                                    <li>techo1</li>
-                                </ul>
-                            </div>
-                            
-                        </div>
-                    </div>
-                    <div class="codeSource">
-                        <p>lien <span>  </span></p>
-                    </div>
-                </div>
+            
+            
        
             
-
         </section>
         <section id="Veille">
         
---
+++ js/main.js
@@ -0,0 +1,7 @@
+let Presentation=document.getElementById("Presentation"); var BTS=document.getElementById("BTS");
+var Realisation=document.getElementById("Realisation");var Veille=document.getElementById("Veille");
+
+function nav(){
+    let show;
+    Presentation.style.visibility="hidden";
+}
